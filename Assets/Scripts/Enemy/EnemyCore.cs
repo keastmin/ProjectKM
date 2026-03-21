a31986f9489d49e43c1bc5133fb45bc505b9744c
keastmin <kemin1910@gmail.com>
2026-03-22 05:03:45 +0900

    적에게 피격시 애니메이션 추가

--- Assets/Scripts/Enemy/EnemyCore.cs
+++ Assets/Scripts/Enemy/EnemyCore.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 
 public class EnemyCore : MonoBehaviour, IDamageable
 {
+    [SerializeField] private Animator _animator;
     [SerializeField] private DamageStatus _damageStatus;
+
+    private const string DAMAGED_NAME = "Damaged";
+    private int _damagedHash;
 
     private Rigidbody _rigidbody;
 
     private void Awake()
     {
         TryGetComponent(out _rigidbody);
+        _damagedHash = Animator.StringToHash("Base Layer." + DAMAGED_NAME);
     }
 
     private void FixedUpdate()
@@ -22,6 +27,11 @@ public class EnemyCore : MonoBehaviour, IDamageable
         if (_damageStatus != null)
         {
             _damageStatus.SetDamage(damage);
+        }
+
+        if(_animator != null)
+        {
+            _animator.CrossFade(_damagedHash, 0.08f, 0, 0f, 0f);
         }
     }
 }
