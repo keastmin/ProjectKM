83e31f6912abba0bd3c329429da879d41a66ef81
keastmin <kemin1910@gmail.com>
2026-05-15 00:09:14 +0900

    scene: Basecamp Scene을 만듦

---
+++ Assets/Scripts/UI/Main/MainMenuUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private string _gameScene;
+
+    public void OnClickNewGameButton()
+    {
+        SceneManager.LoadScene(_gameScene);
+    }
+
+    public void OnClickQuitButton()
+    {
+#if UNITY_EDITOR
+        // Unity 에디터에서 플레이 모드 종료
+        EditorApplication.isPlaying = false;
+#else
+        // 실제 빌드된 게임 종료
+        Application.Quit();
+#endif
+    }
+}
